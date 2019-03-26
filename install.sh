17df6047b2a6c3ea6438f66f8b25d8dd0110a090
高小川 <gaoxiaochuan@hotmail.com>
2019-03-26 15:41:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo yum install -y yum-utils \
   
 yum-config-manager \
   --add-repo \
-  https://download.docker.com/linux/centos/docker-ce.repo
+  http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
   
 yum install docker-ce docker-ce-cli containerd.io -y
 

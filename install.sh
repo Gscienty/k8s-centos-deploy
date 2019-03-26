37b07de76e53797bceb9b6203bbc98edd05cae4f
高小川 <gaoxiaochuan@hotmail.com>
2019-03-26 15:35:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ yum-config-manager \
   --add-repo \
   https://download.docker.com/linux/centos/docker-ce.repo
   
-yum install docker-ce docker-ce-cli containerd.io
+yum install docker-ce docker-ce-cli containerd.io -y
 
 systemctl enable docker && systemctl start docker
 
